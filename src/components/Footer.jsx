710b88b2f09e733824c7f3b3cbe0c819875721d8
Amriteshsingh94 <amriteshsingh2000n@gmail.com>
2024-01-17 18:02:26 +0530

    wip changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
+import MyImg from "../assets/amritesh.jpeg";
 
 const avatarSrc = "";
 const Footer = () => {
@@ -15,7 +16,7 @@ const Footer = () => {
                 </VStack>
 
                 <VStack>
-                    <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+                    <Avatar boxSize={"28"} mt={["4", "0"]} src={MyImg} />
                     <Text>Our Founder</Text>
                 </VStack>
             </Stack>
